0b75f25a889012dcd913adf3b89f4e07c8a797ce
Ben Armstrong <benjamin.armstrong@ons.gov.uk>
2016-12-08 17:14:13 +0000

    Badge notes updated.

--- components/components/badge/badge.config.js
+++ components/components/badge/badge.config.js
@@ -2,5 +2,5 @@ module.exports = {
   "title": "Badge",
   "status": "wip",
   "collated": "true",
-  "notes": "Collated components via badge.config.js."
+  "notes": "May require updating colours to maintain parity with GDS. [GDS Design Notes](https://designnotes.blog.gov.uk/2016/12/07/weve-updated-the-alpha-and-beta-phase-banners/) Collated components via badge.config.js."
 }
