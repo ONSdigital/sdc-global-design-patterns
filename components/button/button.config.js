bbbb943e0088f12ce53f4bf9e1bf2a4a745b7677
Ben Armstrong <benjamin.armstrong@ons.gov.uk>
2017-09-27 17:19:17 +0100

    Rationalising button to use variants from config file and to add Notes.

--- components/button/button.config.js
+++ components/button/button.config.js
@@ -1,6 +1,48 @@
 module.exports = {
   "title": "Button",
   "default": "primary",
-  "status": "wip",
   "collated": true,
+  "status": "ready",
+  "variants": [{
+    "name": "primary",
+    "label": "Primary",
+    "context": {
+      "btn-classes": "btn--primary",
+      "label": "Submit"
+    },
+  }, {
+    "name": "primary-lg",
+    "label": "Primary (Large)",
+    "context": {
+      "btn-classes": "btn--primary btn--lg",
+      "label": "Start survey"
+    },
+  }, {
+    "name": "Secondary",
+    "context": {
+      "btn-classes": "btn--secondary",
+      "label": "Filter results"
+    },
+  }, {
+    "name": "bordered",
+    "label": "Bordered (Secondary)",
+    "context": {
+      "btn-classes": "btn--secondary btn--border",
+      "label": "Add a person"
+    },
+  }, {
+    "name": "link",
+    "label": "Link",
+    "context": {
+      "btn-classes": "btn--link",
+      "label": "Cancel"
+    },
+  }, {
+    "name": "neutral",
+    "label": "Neutral / Disabled",
+    "context": {
+      "btn-classes": "btn--neutral",
+      "label": "Neutral"
+    },
+  }]
 }
