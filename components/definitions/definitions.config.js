34301568afe81e04409e335bcaee66cf102dc4fd
Benn Pearson <bennpearson@gmail.com>
2017-11-29 09:42:16 +0000

    added definition static version

--- components/definitions/definitions.config.js
+++ components/definitions/definitions.config.js
@@ -1,7 +1,22 @@
 module.exports = {
-  title: "Definitions",
-  status: "wip",
+  title: 'Definitions',
+  status: 'wip',
   context: {
-    "definitioncontent": "Do you connect an <a href=\"#\" title=\"Definition of accumulator\" class=\"glossary__link\">accumulator</a> to your <a href=\"#\" title=\"Definition of photovoltaic system\" class=\"glossary__link\">photovoltaic system</a> to store surplus energy?"
+    definitioncontent:
+      'Do you connect an <a href="#definition__accumulator" title="Definition of accumulator" class="definition__anchor">accumulator</a> to your <a href="#definition__photovoltaic" title="Definition of photovoltaic system" class="definition__anchor">photovoltaic system</a> to store surplus energy?',
+    definitions: [
+      {
+        id: 'definition__accumulator',
+        title: 'Definition for an accumulator',
+        description:
+          'An accumulator is an energy storge device; a device which accepts energy, stores energy and releases energy as needed.'
+      },
+      {
+        id: 'definition__photovoltaic',
+        title: 'Definition for a photovoltaic system',
+        description:
+          'Photovoltaics known as solar panels are usually installed on homes and comercial properties. They produce electricity directly from the energy of the sun. The panels are usually installed on the roof of your property.'
+      }
+    ]
   }
-}
+};
