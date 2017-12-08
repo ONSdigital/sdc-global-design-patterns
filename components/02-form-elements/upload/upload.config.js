f7e846046f04a9282ca64402d09de8ba51776373
Ben Armstrong <benjamin.armstrong@ons.gov.uk>
2017-12-08 17:01:48 +0000

    Resolves BEM naming inconsistency and now uses media query helper function

--- components/02-form-elements/upload/upload.config.js
+++ components/02-form-elements/upload/upload.config.js
@@ -2,4 +2,7 @@ module.exports = {
   "title": "Upload",
   "name": "upload",
   "status": "ready",
+    "context": {
+      "upload_accepts": ".xls,.xlsx,.pdf", /* Specify file types to accept via upload */
+    },
 }
