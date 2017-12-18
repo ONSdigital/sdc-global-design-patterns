af1dcdf879cad0d1c39a36e1608a35182d32bcf5
Benn Pearson <bennpearson@gmail.com>
2017-12-18 14:50:53 +0000

    functionality amend

--- components/definitions/definitions.js
+++ components/definitions/definitions.js
@@ -11,18 +11,33 @@ export const attrResponse = 'data-dialog-response';
 export const attrDataDialogCall = 'data-dialog-call';
 export const attrDataTitle = 'data-title';
 
-export default function definitionDialog() {
-  const anchors = document.getElementsByClassName(classAnchor);
+export default function() {
+  return definitionDialog();
+}
+
+export function definitionDialog() {
+  anchorsToButton();
+  buttonClick();
+}
+
+// Replace anchor links with buttons
+export function anchorsToButton() {
+  let anchors = document.getElementsByClassName(classAnchor);
   for (let anchor of anchors) {
     // Replace <a> tag with <button> tag.
     anchor.outerHTML = anchor.outerHTML
       .replace(/<a/g, '<button')
       .replace(/<\/a>/g, '</button>');
   }
+}
+
+// Open dialog
+export function buttonClick() {
   const buttons = document.getElementsByClassName(classAnchor);
   for (let button of buttons) {
     button.onclick = function(e) {
       e.preventDefault();
+
       // define the dialog element
       let dialog = document.getElementsByClassName(classDialog)[0];
 
@@ -31,62 +46,69 @@ export default function definitionDialog() {
         ? button.getAttribute('id')
         : 'trigger';
 
-      // open dialog and add roles
-      dialog.setAttribute('tabindex', '0');
-      dialog.setAttribute('open', 'true');
-      dialog.setAttribute('role', 'alertdialog');
-      dialog.setAttribute('aria-labelledby', idDialogMessage);
+      if (dialog.getAttribute('open') === 'true') {
+        closeDialog(buttons, trigger, dialog);
+      }
 
-      // retrieve custom close button wording, if any
-      let closeText = button.getAttribute(attrResponse)
-        ? button.getAttribute(attrResponse)
-        : 'close';
-
-      // build the dialog markup
-      dialog.insertAdjacentHTML(
-        'beforeend',
-        '<div class="dialog__wrapper">' +
-          '<div class="container">' +
-            '<div class="grid grid--reverse">' +
-              '<div class="grid__col col-4@m">' +
-                '<div class="dialog__document" id="document" role="document" tabindex="0">' +
-                  '<button class="' + classDialogButton + ' mars" id="button" role="button">' +
-                    closeText +
-                  '</button>' +
-                '</div>' +
-              '</div>' +
-              '<div class="grid__col col-7@m pull-1@m">' +
-              '</div>' +
-            '</div>' +
-          '</div>' +
-        '</div>'
-      );
-
-      // make last button in dialog the close button
-      let btnClose = document.getElementsByClassName(classDialogButton)[0];
-      btnClose.focus();
-
-      // Insert the message held in the trigger's [data-dialog-msg] attribute
-      let dataDialogCall = button.getAttribute(attrDataDialogCall);
-      let dataTitle = button.getAttribute(attrDataTitle);
-
-      btnClose.insertAdjacentHTML(
-        'beforebegin',
-        '<div id="' + idDialogMessage + '" class="dialog__message" ><h3 class="dialog__title venus">' + dataTitle + '</h3><p class="dialog__description mars" >' + dataDialogCall + '</p></div>'
-      );
-
-      // Define content to refocus dialog if user tries to leave it
-      let content = document.getElementsByClassName(classDialogDocument)[0];
-
-      closeButton(btnClose, closeDialog, trigger, dialog);
-      escapeEvent(dialog, closeDialog, trigger);
-      focusEvent(btnClose, content);
+      openDialog(button, dialog, trigger);
     };
   }
 }
 
+// Open Dialog
+export function openDialog(button, dialog, trigger) {
+  // retrieve custom close button wording, if any
+  let closeText = button.getAttribute(attrResponse)
+    ? button.getAttribute(attrResponse)
+    : 'close';
+
+  button.setAttribute('disabled', 'true');
+
+  // open dialog and add roles
+  dialog.setAttribute('tabindex', '0');
+  dialog.setAttribute('open', 'true');
+  dialog.setAttribute('role', 'alertdialog');
+  dialog.setAttribute('aria-labelledby', idDialogMessage);
+
+  // build the dialog markup
+  dialog.insertAdjacentHTML(
+    'beforeend',
+    '<div class="dialog__wrapper">' +
+      '<div class="' + classDialogDocument + '" id="document" role="document" tabindex="0">' +
+        '<button class="' + classDialogButton + ' mars" id="button" role="button">' +
+          closeText +
+        '</button>' +
+      '</div>' +
+    '</div>'
+  );
+
+  // Define content to refocus dialog if user tries to leave it
+  let content = document.getElementsByClassName(classDialogDocument)[0];
+
+  // make last button in dialog the close button
+  let btnClose = document.getElementsByClassName(classDialogButton)[0];
+  btnClose.focus();
+
+  // Insert the message held in the trigger's [data-dialog-msg] attribute
+  let dataDialogCall = button.getAttribute(attrDataDialogCall);
+  let dataTitle = button.getAttribute(attrDataTitle);
+
+  btnClose.insertAdjacentHTML(
+    'beforebegin',
+    '<div id="' + idDialogMessage + '" class="dialog__message" ><h3 class="dialog__title venus">' + dataTitle + '</h3><p class="dialog__description mars" >' + dataDialogCall + '</p></div>'
+  );
+
+  closeButton(btnClose, closeDialog, trigger, dialog);
+  escapeEvent(dialog, closeDialog, trigger);
+  focusEvent(btnClose, content);
+}
+
 // Close Dialog
 export function closeDialog(trigger, dialog) {
+  const buttons = document.getElementsByClassName(classAnchor);
+  for (let button of buttons) {
+    button.removeAttribute('disabled');
+  }
   document.getElementById(idDialogMessage).remove();
 
   // Set focus back to element that triggered dialog
@@ -107,7 +129,7 @@ export function closeDialog(trigger, dialog) {
   }
 }
 
-// run closeDialog() on click of close button
+// run closeDialog() on click of anchor button
 export function closeButton(btnClose, closeDialog, trigger, dialog) {
   btnClose.onclick = function(e) {
     closeDialog(trigger, dialog);
