feba23617dd07166dbb0d1712b1388422a0ab911
Benn Pearson <bennpearson@gmail.com>
2017-12-22 08:44:37 +0000

    resolved 2 conflicts

--- components/02-form-elements/input/input.config.js
+++ components/02-form-elements/input/input.config.js
@@ -1,44 +1,50 @@
 module.exports = {
-  "title": "Input",
-  "default": "text",
-  "collated": false,
-  "status": "ready",
-  "variants": [{
-    "name": "text",
-    "label": "Text",
-    "context": {
-      "label": "Textfield",
-      "label_text": "Enter some text",
-      "label_for": "text-input",
-      "label_inline": false
-    },
+  'title': 'Input',
+  'default': 'text',
+  'collated': false,
+  'status': 'ready',
+  'variants': [{
+    'name': 'text',
+    'label': 'Text',
+    'context': {
+      'label': 'Textfield',
+      'label_text': 'Enter some text',
+      'label_for': 'text-input',
+      'label_inline': false
+    }
   }, {
-    "name": "checkbox",
-    "context": {
-      "label_text": "Bacon",
-      "label_for": "checkbox",
-      "label_inline": true
-    },
+    'name': 'checkbox',
+    'context': {
+      'label_text': 'Bacon',
+      'label_for': 'checkbox',
+      'label_inline': true
+    }
   }, {
-    "name": "radio",
-    "status": "ready",
-    "context": {
-      "label_text": "Bacon",
-      "label_for": "radio",
-      "label_inline": true,
-      "legend":"Select your favourite topping",
-      "options": [
-        {"option_text":"Bacon", "option_value": "bacon", "label_for":"bacon", "label_text":"Bacon", "label_inline":true},
-        {"option_text":"Olives", "option_value": "olives", "label_for":"olives", "label_text":"Olives", "label_inline":true},
+    'name': 'radio',
+    'status': 'ready',
+    'context': {
+      'label_text': 'Bacon',
+      'label_for': 'radio',
+      'label_inline': true,
+      'legend': 'Select your favourite topping',
+      'options': [
+        {'option_text': 'Bacon', 'option_value': 'bacon', 'label_for': 'bacon', 'label_text': 'Bacon', 'label_inline': true},
+        {'option_text': 'Olives', 'option_value': 'olives', 'label_for': 'olives', 'label_text': 'Olives', 'label_inline': true}
       ]
-    },
+    }
   }, {
-    "name": "percentage",
-    "status": "ready",
-    "name": "percentage",
-    "context": {
-      "label": "Percentage",
-      "label_text": "%"
+    'name': 'percentage',
+    'status': 'ready',
+    'context': {
+      'label': 'Percentage',
+      'label_text': '%'
+    }
+  }, {
+    'name': 'password',
+    'status': 'wip',
+    'context': {
+      'label': 'Password',
+      'label_text': 'Enter password'
     }
   }]
-}
+};
