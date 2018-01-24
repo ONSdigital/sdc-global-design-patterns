4d8a21d544409c388f5a0b18bd55ec58917d2fe5
Shane Edwards <shane.edwards9@gmail.com>
2018-01-24 17:02:56 +0000

    Component tidy for session timeout

--- components/countdown/countdown.config.js
+++ components/countdown/countdown.config.js
@@ -3,6 +3,7 @@ module.exports = {
   "status": "wip",
   "collated": false,
   "notes": "Collated components via countdownAnimation.config.js",
+  hidden: true,
   context: {
     width: '40',
     widthDouble: '80'
