fe7d0a87d8e7b78cdba7d6483a6c5515015b56db
Ben Armstrong <benjamin.armstrong@ons.gov.uk>
2018-01-30 18:04:49 +0000

    Corrected highlight to use em and updated notes for usage

--- components/highlight/highlight.config.js
+++ components/highlight/highlight.config.js
@@ -2,9 +2,9 @@ module.exports = {
   title: "Highlight",
   status: "ready",
   context: {
-    "highlightcontent": "This is some <span class=\"highlight\">important</span> content.",
-    "highlightcontent-heading": "What was the value of the business’s total sales of <span class=\"highlight\">food</span>?",
-    "highlightcontent-heading2": "What was the value of the business’s total sales of <span class=\"highlight\">alcohol, tobacco and confectionary</span>?",
-    "highlightcontent-total": "Of the <span class=\"highlight\">423</span> employees how many were part-time?"
+    "highlightcontent": "This is some <em class=\"highlight\">important</em> content.",
+    "highlightcontent-heading": "What was the value of the business’s total sales of <em class=\"highlight\">food</em>?",
+    "highlightcontent-heading2": "What was the value of the business’s total sales of <em class=\"highlight\">alcohol, tobacco and confectionary</em>?",
+    "highlightcontent-total": "Of the <em class=\"highlight\">423</em> employees how many were part-time?"
   }
 }
