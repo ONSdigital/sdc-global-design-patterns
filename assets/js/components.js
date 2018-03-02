b1f705fdd58f194846897d0f8c1371e67a281268
Shane Edwards <shane.edwards9@gmail.com>
2018-03-02 15:28:41 +0000

    Added missing javascript modules from EQ

--- assets/js/components.js
+++ assets/js/components.js
@@ -2,6 +2,7 @@ import 'babel-polyfill';
 
 import './focus-styles';
 import './dialog';
+import './submitter';
 // Character count component (textarea)
 import '../../components/02-form-elements/textarea/character-limit.js';
 import '../../components/definitions/definitions.js';
@@ -9,3 +10,4 @@ import '../../components/02-form-elements/input/input--password';
 import '../../components/timeout/timeout.js';
 import '../../components/accordion/accordion.js';
 import '../../components/guidance/details-toggle.js';
+import '../../components/04-navigation/nav.js';
