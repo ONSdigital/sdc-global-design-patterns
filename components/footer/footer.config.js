417ab71112a9674d93a1d79f788b8eecc6adab83
Ben Armstrong <benjamin.armstrong@ons.gov.uk>
2018-03-23 16:00:30 +0000

    Initial work to produce the eq variant of the footer.

--- components/footer/footer.config.js
+++ components/footer/footer.config.js
@@ -2,5 +2,28 @@ module.exports = {
   "title": "Footer",
   "status": "ready",
   "collated": false,
-  "preview": "@preview__page" /* As it is a layout level element use page preview */
+  "preview": "@preview__page", /* As it is a layout level element use page preview */
+  "default": "primary",
+  "variants": [{
+    "name": "primary",
+    "label": "Primary",
+    "context": {
+    },
+  }, {
+    "name": "eq",
+    "label": "EQ",
+    "status": "wip",
+    "context": {
+      "links":[
+      {
+        "link_text": "Contact us",
+        "link_url": "/contact-us"
+      },
+      {
+        "link_text": "Cookies and privacy",
+        "link_url": "/cookies-privacy"
+      }],
+    },
+  }
+]
 }
