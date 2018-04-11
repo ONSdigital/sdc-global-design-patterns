195c7f2d2a1cba7e3fb4272a2f2211130f8b6b7d
Benn Pearson <bennpearson@gmail.com>
2018-04-11 13:02:04 +0100

    simplified js

--- components/02-form-elements/mutually-exclusive/mutually-exclusive.js
+++ components/02-form-elements/mutually-exclusive/mutually-exclusive.js
@@ -1,27 +1,24 @@
 import domready from '../../../assets/js/domready';
 
-const exclusiveCheckboxClass = 'js-exclusive-checkboxes';
+const exclusiveCheckboxGroupClass = 'js-exclusive-checkbox-group';
+const exclusiveCheckboxClass = 'js-exclusive-checkbox';
 
 export default function mutuallyExclusiveCheckboxes() {
-  const exclusiveCheckboxElements = document.getElementsByClassName(exclusiveCheckboxClass);
-  for (let exclusiveCheckboxElement of exclusiveCheckboxElements) {
-    exclusiveCheckboxElement.onclick = function() {
-      if (exclusiveCheckboxElement.getAttribute('value') === 'none') {
-        if (exclusiveCheckboxElement.checked === true) {
-          for (let exclusiveCheckboxElement of exclusiveCheckboxElements) {
-            exclusiveCheckboxElement.checked = false;
-          }
-          exclusiveCheckboxElement.checked = true;
-        } else {
-        }
-      } else {
-        if (exclusiveCheckboxElement.checked === true) {
-          for (let exclusiveCheckboxElement of exclusiveCheckboxElements) {
-            exclusiveCheckboxElement.checked = false;
-          }
-          exclusiveCheckboxElement.checked = true;
-        } else {
-        }
+  const exclusiveCheckboxGroupElements = document.getElementsByClassName(exclusiveCheckboxGroupClass);
+  const exclusiveCheckboxElement = document.getElementsByClassName(exclusiveCheckboxClass);
+  for (let exclusiveCheckboxGroupElement of exclusiveCheckboxGroupElements) {
+    exclusiveCheckboxGroupElement.onclick = function() {
+      if (exclusiveCheckboxElement[0].checked === true) {
+        exclusiveCheckboxElement[0].checked = false;
+        exclusiveCheckboxElement[0].parentElement.classList.remove('is-checked');
+      }
+    }
+  }
+  exclusiveCheckboxElement[0].onclick = function() {
+    for (let exclusiveCheckboxGroupElement of exclusiveCheckboxGroupElements) {
+      if (exclusiveCheckboxGroupElement.checked === true) {
+        exclusiveCheckboxGroupElement.checked = false;
+        exclusiveCheckboxGroupElement.parentElement.classList.remove('is-checked');
       }
     }
   }
