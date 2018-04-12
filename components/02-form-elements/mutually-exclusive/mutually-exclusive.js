a54d680f71bde43bc811ef07b1b4061d88748ea0
Benn Pearson <bennpearson@gmail.com>
2018-04-12 09:58:07 +0100

    added a11y visually hidden feedback

--- components/02-form-elements/mutually-exclusive/mutually-exclusive.js
+++ components/02-form-elements/mutually-exclusive/mutually-exclusive.js
@@ -2,23 +2,29 @@ import domready from '../../../assets/js/domready';
 
 const exclusiveCheckboxGroupClass = 'js-exclusive-checkbox-group';
 const exclusiveCheckboxClass = 'js-exclusive-checkbox';
+const exclusiveCheckboxAlertClass = 'js-exclusive-checkbox-alert';
 
 export default function mutuallyExclusiveCheckboxes() {
   const exclusiveCheckboxGroupElements = document.getElementsByClassName(exclusiveCheckboxGroupClass);
   const exclusiveCheckboxElement = document.getElementsByClassName(exclusiveCheckboxClass);
+  const exclusiveCheckboxAlert = document.getElementsByClassName(exclusiveCheckboxAlertClass);
   for (let exclusiveCheckboxGroupElement of exclusiveCheckboxGroupElements) {
     exclusiveCheckboxGroupElement.onclick = function() {
+      exclusiveCheckboxAlert[0].innerHTML = '';
       if (exclusiveCheckboxElement[0].checked === true) {
         exclusiveCheckboxElement[0].checked = false;
         exclusiveCheckboxElement[0].parentElement.classList.remove('is-checked');
+        exclusiveCheckboxAlert[0].append(exclusiveCheckboxElement[0].getAttribute('value') + ' deselected. ');
       }
     }
   }
   exclusiveCheckboxElement[0].onclick = function() {
+    exclusiveCheckboxAlert[0].innerHTML = '';
     for (let exclusiveCheckboxGroupElement of exclusiveCheckboxGroupElements) {
       if (exclusiveCheckboxGroupElement.checked === true) {
         exclusiveCheckboxGroupElement.checked = false;
         exclusiveCheckboxGroupElement.parentElement.classList.remove('is-checked');
+        exclusiveCheckboxAlert[0].append(exclusiveCheckboxGroupElement.getAttribute('value') + ' deselected. ');
       }
     }
   }
