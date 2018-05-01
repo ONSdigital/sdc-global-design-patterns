3e19d13d98cc701e71e2bfdf12f799996ad7a82f
Benn Pearson <bennpearson@gmail.com>
2018-05-01 15:10:16 +0100

    ie10 fix

--- components/02-form-elements/mutually-exclusive/mutually-exclusive.js
+++ components/02-form-elements/mutually-exclusive/mutually-exclusive.js
@@ -30,7 +30,7 @@ const checkboxToggle = function(checkboxElement, checkboxAlertElement) {
   if (checkboxElement.checked === true) {
     checkboxElement.checked = false;
     checkboxElement.parentElement.classList.remove('is-checked');
-    checkboxAlertElement.append(checkboxElement.getAttribute('value') + ' ' + checkboxAlertElement.dataset.adjective + '. ');
+    checkboxAlertElement.append(checkboxElement.getAttribute('value') + ' ' + checkboxAlertElement.getAttribute('data-adjective') + '. ');
   }
 }
 
