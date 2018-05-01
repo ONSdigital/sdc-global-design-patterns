eae50fed14048b728f6ab97164f9b2dde07fd4bc
Benn Pearson <bennpearson@gmail.com>
2018-05-01 14:46:54 +0100

    multiple instances now possible

--- components/02-form-elements/mutually-exclusive/mutually-exclusive.js
+++ components/02-form-elements/mutually-exclusive/mutually-exclusive.js
@@ -1,23 +1,27 @@
 import domready from '../../../assets/js/domready';
 
+const exclusiveCheckboxWrapperClass = 'field--exclusive'
 const exclusiveCheckboxGroupClass = 'js-exclusive-checkbox-group';
 const exclusiveCheckboxClass = 'js-exclusive-checkbox';
 const exclusiveCheckboxAlertClass = 'js-exclusive-checkbox-alert';
 
 export default function mutuallyExclusiveCheckboxes() {
-  const exclusiveCheckboxGroupElements = document.getElementsByClassName(exclusiveCheckboxGroupClass);
-  const exclusiveCheckboxElement = document.getElementsByClassName(exclusiveCheckboxClass);
-  const exclusiveCheckboxAlert = document.getElementsByClassName(exclusiveCheckboxAlertClass);
-  for (let exclusiveCheckboxGroupElement of exclusiveCheckboxGroupElements) {
-    exclusiveCheckboxGroupElement.onclick = function() {
-      exclusiveCheckboxAlert[0].innerHTML = '';
-      checkboxToggle(exclusiveCheckboxElement[0], exclusiveCheckboxAlert[0]);
-    }
-  }
-  exclusiveCheckboxElement[0].onclick = function() {
-    exclusiveCheckboxAlert[0].innerHTML = '';
+  const exclusiveCheckboxWrapperElements = document.getElementsByClassName(exclusiveCheckboxWrapperClass);
+  for (let exclusiveCheckboxWrapperElement of exclusiveCheckboxWrapperElements) {
+    let exclusiveCheckboxGroupElements = exclusiveCheckboxWrapperElement.getElementsByClassName(exclusiveCheckboxGroupClass);
+    let exclusiveCheckboxElement = exclusiveCheckboxWrapperElement.getElementsByClassName(exclusiveCheckboxClass);
+    let exclusiveCheckboxAlert = exclusiveCheckboxWrapperElement.getElementsByClassName(exclusiveCheckboxAlertClass);
     for (let exclusiveCheckboxGroupElement of exclusiveCheckboxGroupElements) {
-      checkboxToggle(exclusiveCheckboxGroupElement, exclusiveCheckboxAlert[0]);
+      exclusiveCheckboxGroupElement.onclick = function() {
+        exclusiveCheckboxAlert[0].innerHTML = '';
+        checkboxToggle(exclusiveCheckboxElement[0], exclusiveCheckboxAlert[0]);
+      }
+    }
+    exclusiveCheckboxElement[0].onclick = function() {
+      exclusiveCheckboxAlert[0].innerHTML = '';
+      for (let exclusiveCheckboxGroupElement of exclusiveCheckboxGroupElements) {
+        checkboxToggle(exclusiveCheckboxGroupElement, exclusiveCheckboxAlert[0]);
+      }
     }
   }
 }
