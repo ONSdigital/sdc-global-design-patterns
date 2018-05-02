883e24dabd0db0792affa144672c3587d30fa3f4
Benn Pearson <bennpearson@gmail.com>
2018-05-02 10:49:37 +0100

    removed bracket notation

--- components/02-form-elements/mutually-exclusive/mutually-exclusive.js
+++ components/02-form-elements/mutually-exclusive/mutually-exclusive.js
@@ -1,36 +1,36 @@
-import domready from 'assets/js/domready';
+import domready from '/assets/js/domready';
 
 const checkboxWrapperClass = 'field--exclusive'
 const checkboxGroupClass = 'js-exclusive-checkbox-group';
 const checkboxClass = 'js-exclusive-checkbox';
-const spanAlertClass = 'js-exclusive-checkbox-alert';
+const voiceOverAlertClass = 'js-exclusive-checkbox-alert';
 
 export default function mutuallyExclusiveCheckboxes() {
   const checkboxWrapperElements = document.getElementsByClassName(checkboxWrapperClass);
   for (let checkboxWrapperElement of checkboxWrapperElements) {
     const checkboxGroupElements = checkboxWrapperElement.getElementsByClassName(checkboxGroupClass);
     const checkboxElement = checkboxWrapperElement.getElementsByClassName(checkboxClass);
-    const spanAlert = checkboxWrapperElement.getElementsByClassName(spanAlertClass);
+    const voiceOverAlertElement = checkboxWrapperElement.getElementsByClassName(voiceOverAlertClass);
     for (let checkboxGroupElement of checkboxGroupElements) {
       checkboxGroupElement.addEventListener('change', function() {
-        spanAlert[0].innerHTML = '';
-        checkboxToggle(checkboxElement[0], spanAlert[0]);
+        voiceOverAlertElement.innerHTML = '';
+        checkboxToggle(checkboxElement, voiceOverAlertElement);
       });
     }
-    checkboxElement[0].addEventListener('change', function() {
-      spanAlert[0].innerHTML = '';
+    checkboxElement.addEventListener('change', function() {
+      voiceOverAlertElement.innerHTML = '';
       for (let checkboxGroupElement of checkboxGroupElements) {
-        checkboxToggle(checkboxGroupElement, spanAlert[0]);
+        checkboxToggle(checkboxGroupElement, voiceOverAlertElement);
       }
     });
   }
 }
 
-const checkboxToggle = function(checkboxEl, spanAlertEl) {
+const checkboxToggle = function(checkboxEl, voiceOverAlertEl) {
   if (checkboxEl.checked === true) {
     checkboxEl.checked = false;
     checkboxEl.parentElement.classList.remove('is-checked');
-    spanAlertEl.append(checkboxEl.getAttribute('value') + ' ' + spanAlertEl.getAttribute('data-adjective') + '. ');
+    voiceOverAlertEl.append(checkboxEl.getAttribute('value') + ' ' + voiceOverAlertEl.getAttribute('data-adjective') + '. ');
   }
 }
 
