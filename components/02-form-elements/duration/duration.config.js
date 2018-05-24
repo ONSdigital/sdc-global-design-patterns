90b7bb14a635f923542e9d11dfd6e9a0152e6211
boxadesign <info@boxadesign.co.uk>
2018-05-24 14:45:25 +0100

    Cleaned up classes and split duration children into two types

--- components/02-form-elements/duration/duration.config.js
+++ components/02-form-elements/duration/duration.config.js
@@ -3,12 +3,19 @@ module.exports = {
   'collated': false,
   'status': 'wip',
   'context': {
-    'label': 'Time',
+    'label_time': 'Time',
     'unit1': 'Hours',
     'unit1Title': 'Hours',
     'unit2': 'Mins',
     'unit2Title': 'Minutes',
     'unit3': 'Secs',
-    'unit3Title': 'Seconds'
+    'unit3Title': 'Seconds',
+    'label_period': 'Period',
+    'unit4': 'Days',
+    'unit4Title': 'Days',
+    'unit5': 'Months',
+    'unit5Title': 'Months',
+    'unit6': 'Years',
+    'unit6Title': 'Years'
   }
 };
