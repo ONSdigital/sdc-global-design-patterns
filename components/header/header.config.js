5e44fa159520be5bba31ae492db36c129c685968
boxadesign <info@boxadesign.co.uk>
2018-06-12 09:33:39 +0100

    Updated header with new styles and variants

--- components/header/header.config.js
+++ components/header/header.config.js
@@ -1,6 +1,29 @@
 module.exports = {
   "title": "Header",
   "status": "ready",
-  "collated": false,
-  "preview": "@preview__page" /* As it is a layout level element use page preview */
+  "default": "standard",
+  "collated": true,
+  "preview": "@preview__page", /* As it is a layout level element use page preview */
+  "variants": [{
+    "name": "standard",
+    "label": "Header - standard",
+    "context": {
+      "title": "Standard title header"
+    },
+  },{
+    "name": "hero",
+    "label": "Header - hero",
+    "context": {
+      "title": "Hero title header",
+      "classes": "header--hero",
+      "description": "This is the description or tagline"
+    },
+  },{
+    "name": "thin",
+    "label": "Header - thin",
+    "context": {
+      "title": "Thin title header",
+      "classes": "header--thin",
+    },
+  }]
 }
