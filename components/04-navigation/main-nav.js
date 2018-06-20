122bbca635f0885317dea22cf0e967d201500eea
boxadesign <info@boxadesign.co.uk>
2018-06-20 14:50:23 +0100

    Fixed issue with mobile nav and tabindex

--- components/04-navigation/main-nav.js
+++ components/04-navigation/main-nav.js
@@ -1,16 +1,30 @@
 import domready from '../../assets/js/domready'
+import {forEach} from 'lodash'
 
 const attrExpanded = 'aria-expanded'
 const attrHidden = 'aria-hidden'
+const tabIndex = 'tabindex'
 const hideClass = 'nav--h-m'
 const activeClass = 'active'
 
 let navVisible = false
 
 export default class Nav {
-    constructor(btn, nav) {
-        this.btn = btn
+    constructor(toggle, nav) {
+        this.toggle = toggle
         this.nav = nav
+        const list = this.nav.childNodes[1]
+        const links = list.getElementsByTagName('a')
+        const btns = list.getElementsByTagName('button')
+        this.links = links
+        this.btns = btns
+    }
+
+    checkState() {
+        if (!navVisible) {
+            forEach(this.links, link => { link.setAttribute(tabIndex, '-1') })
+            forEach(this.btns, btn => { btn.setAttribute(tabIndex, '-1') })
+        }
     }
 
     toggleNav() {
@@ -18,29 +32,34 @@ export default class Nav {
     }
 
     openNav() {
-        this.btn.classList.add(activeClass)
-        this.btn.setAttribute(attrExpanded, 'true')
+        this.toggle.classList.add(activeClass)
+        this.toggle.setAttribute(attrExpanded, 'true')
         this.nav.setAttribute(attrHidden, 'false')
         this.nav.classList.remove(hideClass)
+        forEach(this.links, link => { link.setAttribute(tabIndex, '0') })
+        forEach(this.btns, btn => { btn.setAttribute(tabIndex, '0') })
         navVisible = true
     }
 
     closeNav() {
-        this.btn.classList.remove(activeClass)
-        this.btn.setAttribute(attrExpanded, 'false')
+        this.toggle.classList.remove(activeClass)
+        this.toggle.setAttribute(attrExpanded, 'false')
         this.nav.setAttribute(attrHidden, 'true')
         this.nav.classList.add(hideClass) 
+        forEach(this.links, link => { link.setAttribute(tabIndex, '-1') })
+        forEach(this.btns, btn => { btn.setAttribute(tabIndex, '-1') })
         navVisible = false
     }
 }
 
 domready(() => {
-    const btn = document.querySelector('.js-nav-btn')
+    const toggle = document.querySelector('.js-nav-btn')
     const nav = document.querySelector('.js-main-nav')
 
-    if (!btn || !nav) return false
+    if (!toggle || !nav) return false
 
-    const mainNav = new Nav(btn,nav)
+    const mainNav = new Nav(toggle,nav)
     
-    btn.addEventListener('click', e => mainNav.toggleNav(e))
+    mainNav.checkState()
+    toggle.addEventListener('click', e => mainNav.toggleNav(e))
 })
