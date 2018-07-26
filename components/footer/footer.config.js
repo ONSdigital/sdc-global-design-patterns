405cf487612724dd0e6124cb7e2144674ad9cf73
Ben Armstrong <benjamin.armstrong@ons.gov.uk>
2018-07-26 17:12:11 +0100

    Component footer (light) (#127)
    * Light grey footers, including powered by variants and internal

--- components/footer/footer.config.js
+++ components/footer/footer.config.js
@@ -1,6 +1,84 @@
 module.exports = {
-  'title': 'Footer',
-  'status': 'ready',
-  'collated': false,
-  'preview': '@preview__page' /* As it is a layout level element use page preview */
+  "title": "Footer",
+  "status": "ready",
+  "collated": true,
+  "preview": "@preview__page", /* As it is a layout level element use page preview */
+  "default": "full",
+  "variants": [{
+    "name": "full",
+    "label": "Full",
+    "status": "ready",
+    "context": {
+    },
+  }, {
+    "name": "transactional",
+    "label": "Transactional",
+    "status": "wip",
+    "context": {
+      "links":[
+      {
+        "link_text": "Contact us",
+        "link_url": "/contact-us"
+      },
+      {
+        "link_text": "Cookies and privacy",
+        "link_url": "/cookies-privacy"
+      }],
+    },
+  }, {
+    "name": "internal-service",
+    "label": "Internal service",
+    "status": "wip",
+    "context": {
+      "links":[
+      {
+        "link_text": "Contact us",
+        "link_url": "/contact-us"
+      },
+      {
+        "link_text": "Page generated: 09:39 on 15 June 2018"
+      },
+      {
+        "link_text": "Service owner: SDC"
+      }
+    ],
+    },
+  }, {
+    "name": "poweredbyons",
+    "label": "Powered by ONS",
+    "status": "wip",
+    "context": {
+    },
+  }, {
+    "name": "poweredbyons-transactional",
+    "label": "Transactional - Powered by ONS",
+    "status": "wip",
+    "context": {
+      "links":[
+      {
+        "link_text": "Contact us",
+        "link_url": "/contact-us"
+      },
+      {
+        "link_text": "Cookies and privacy",
+        "link_url": "/cookies-privacy"
+      }],
+    },
+  }, {
+    "name": "poweredbyons-transactional-cy",
+    "label": "Transactional (Cymraeg) - Powered by ONS",
+    "status": "wip",
+    "context": {
+      "links":[
+      {
+        "link_text": "Cysyllwch a ni",
+        "link_url": "/contact-us"
+      },
+      {
+        "link_text": "Cwcis ac preifatrwydd",
+        "link_url": "/cookies-privacy"
+      }],
+    },
+  }
+]
 }
