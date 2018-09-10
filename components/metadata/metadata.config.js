2046b938fc037ec2fba1e598bf5382b614b787c7
Ben Armstrong <benjamin.armstrong@ons.gov.uk>
2018-09-10 14:59:14 +0100

    metadata files

---
+++ components/metadata/metadata.config.js
@@ -0,0 +1,59 @@
+module.exports = {
+  'status': 'wip',
+  'collated': true,
+  'title': 'Metadata',
+  'name':'metadata',
+  'variants':[{
+    'name': 'default',
+    'context' : {
+    'metadata_items':[
+    {
+      'metadata_item': 'Survey:',
+      'metadata_item_col': '3',
+      'metadata_value': ['Bricks &amp; Blocks'],
+      'metadata_value_col': '9',
+    },
+    {
+      'metadata_item': 'RU Ref:',
+      'metadata_item_col': '3',
+      'metadata_value': ['49900000118'],
+      'metadata_value_col': '9',
+    },
+    {
+      'metadata_item': 'Business:',
+      'metadata_item_col': '3',
+      'metadata_value': ['Bolts &amp; Ratchets Ltd.'],
+      'metadata_value_col': '9',
+    },
+    {
+      'metadata_item': 'Trading as:',
+      'metadata_item_col': '3',
+      'metadata_value': ['Bolts &amp; Ratchets'],
+      'metadata_value_col': '9',
+    },{
+      'metadata_item': 'To:',
+      'metadata_item_col': '3',
+      'metadata_values': ['Jacky Turner','Louise Goodland'],
+      'metadata_value_col': '9',
+    }],
+  }
+},{
+  'name': 'message',
+  'label': 'Message',
+  'context' : {
+  'metadata_items':[
+  {
+    'metadata_item': 'From:',
+    'metadata_value': ['Jacky Turner'],
+    'metadata_value_class': 'venus',
+  },
+  {
+    'metadata_item': 'Sent:',
+    'metadata_value': ['Fri 30 Jun 2017 at 11:47'],
+    'metadata_value_class': 'metadata__value--datetime pluto',
+  }],
+  'metadata_label': 'Message metadata',
+  'metadata_label_hidden': true
+}
+},]
+}
