6323cff6a3214e2660e5492b6aea5a5829b352d4
Ben Armstrong <benjamin.armstrong@ons.gov.uk>
2018-10-12 18:41:53 +0100

    Updated component to record Google Analytics events on toggle

--- components/02-form-elements/input/input--password.js
+++ components/02-form-elements/input/input--password.js
@@ -9,10 +9,23 @@ export default function() {
 
 export function bindInputChangeHandlers() {
   if (document.getElementById(idPasswordInputCheckbox)) {
+
     const passwordInputCheckbox = document.getElementById(idPasswordInputCheckbox);
     const passwordInputText = document.getElementById(idPasswordInputText);
+
+// Toggle the data-ga-label to record the hide password event as well as show
+    function toggleGALabel() {
+      var passwordInputGALabel = document.getElementById(idPasswordInputCheckbox).getAttribute('data-ga-label');
+      if(passwordInputGALabel === 'Show password'){
+        passwordInputCheckbox.setAttribute('data-ga-label', 'Hide password');
+      } else {
+        passwordInputCheckbox.setAttribute('data-ga-label', 'Show password');
+      }
+    }
+//
     passwordInputCheckbox.addEventListener('change', () => {
       passwordInputText.type = passwordInputCheckbox.checked ? 'text' : 'password';
+      toggleGALabel();
     });
   }
 }
