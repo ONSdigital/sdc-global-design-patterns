07e9fd54754c05d07491005a158ba09c17204b7f
Ben Armstrong <benjamin.armstrong@ons.gov.uk>
2018-10-15 14:10:04 +0100

    Additional examples in tab config file

--- components/tabs/tabs.config.js
+++ components/tabs/tabs.config.js
@@ -9,24 +9,34 @@ module.exports = {
     {
       'tab': 'UKIS',
       'tabContent' : `<h3>Aim of this survey</h3>
-<p class="mars">The aim of the UK Innovation Survey (UKIS) is to collect data from businesses about various aspects of their innovation related activities. Using this data we can measure the level, types and trends in innovation.</p>
-<h3>How we’ll use this data</h3>
-<p class="mars">The UKIS data is a major source of evidence to inform government policy. It is used to promote innovation activities among businesses to boost economic growth. It is an important contribution to the European-wide Community Innovation Survey (CIS). The CIS is used for international benchmarking and comparison purposes.</p>
-<p><a href="https://www.ons.gov.uk/surveys/informationforbusinesses/businesssurveys/ukinnovationsurvey">More information on the UKIS survey</a> can be found on the ONS website.`
+        <p class="mars">The aim of the UK Innovation Survey (UKIS) is to collect data from businesses about various aspects of their innovation related activities. Using this data we can measure the level, types and trends in innovation.</p>
+        <h3>How we’ll use this data</h3>
+        <p class="mars">The UKIS data is a major source of evidence to inform government policy. It is used to promote innovation activities among businesses to boost economic growth. It is an important contribution to the European-wide Community Innovation Survey (CIS). The CIS is used for international benchmarking and comparison purposes.</p>
+        <p><a href="https://www.ons.gov.uk/surveys/informationforbusinesses/businesssurveys/ukinnovationsurvey">More information on the UKIS survey</a> can be found on the ONS website.`
     },
     {
       'tab': 'Vacancy survey',
       'tabContent' : `<h3>Purpose</h3>
-<p class="mars">The Vacancy Survey is a regular survey of businesses, which provides an accurate and comprehensive measure of the total number of vacancies across the economy and fills a gap in the information available regarding the demand for labour. Before the Vacancy Survey was introduced, the only information available nationally about vacancies was from records of vacancies notified to Job Centres by employers. This provided only a partial picture, possibly less than half of all vacancies, because employers are under no obligation to notify vacancies to Job Centres. This business based survey has a more complete coverage and is included in the monthly ONS Labour Market Statistical Bulletin.</p>
-<p><a href="https://www.ons.gov.uk/surveys/informationforbusinesses/businesssurveys/vacancysurvey">More information on the Vacancy Survey</a> can be found on the ONS website.`
+        <p class="mars">The Vacancy Survey is a regular survey of businesses, which provides an accurate and comprehensive measure of the total number of vacancies across the economy and fills a gap in the information available regarding the demand for labour. Before the Vacancy Survey was introduced, the only information available nationally about vacancies was from records of vacancies notified to Job Centres by employers. This provided only a partial picture, possibly less than half of all vacancies, because employers are under no obligation to notify vacancies to Job Centres. This business based survey has a more complete coverage and is included in the monthly ONS Labour Market Statistical Bulletin.</p>
+        <p><a href="https://www.ons.gov.uk/surveys/informationforbusinesses/businesssurveys/vacancysurvey">More information on the Vacancy Survey</a> can be found on the ONS website.`
     },
     {
-      'tab': 'Third',
-      'tabContent' : 'Third content of tabs region'
+      'tab': 'QBS',
+      'tabContent' : `<h3>Aim of this survey</h3>
+        <p class="mars">The Quarterly Business Survey (QBS) collects quarterly information on employment of businesses in Great Britain. Your response contributes to Labour Market Statistics.</p>
+        <h4>What you need to know</h4>
+        <p class="mars">To complete the survey, you will need the following information to answer the survey questions:</p>
+        <ul>
+        <li>number of full-time/part-time male employees</li>
+        <li>number of full-time/part-time female employees</li>
+        <li>total number of employees for the business</li>
+        </ul>
+        <p class="mars">All information you provide is strictly confidential.</p>
+        <p><a href="https://www.ons.gov.uk/surveys/informationforbusinesses/businesssurveys/quarterlybusinesssurvey">More information on the Vacancy Survey</a> can be found on the ONS website.`
     },
     {
-      'tab': 'The other thing',
-      'tabContent' : 'No. 4'
+      'tab': 'The other tab',
+      'tabContent' : `<p class="mars">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>`
     }],
   }
 }
