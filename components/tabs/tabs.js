8c0a8a92c614ab5609c45448fee11297ed88cde7
Ben Armstrong <benjamin.armstrong@ons.gov.uk>
2018-10-22 12:12:15 +0100

    Investigating mq scripting.

--- components/tabs/tabs.js
+++ components/tabs/tabs.js
@@ -1,9 +1,34 @@
 // Tabs
-import {forEach} from 'lodash'
-import domready from '../../assets/js/domready';
+// import {forEach} from 'lodash'
+// import domready from '../../assets/js/domready';
+//
+// export default function tabs() {
+//   const elTabs = document.querySelectorAll('[role="tablist"]');
+//
+//   forEach(elTabs, element => new Tabs().registerDom(element))
+// }
+// var mediaQueryList = window.matchMedia("(orientation: portrait)");
+//
+// if (mediaQueryList.matches) {
+//   console.log('orientation: portrait');
+// } else {
+//   console.log('orientation: landscape');
+// }
 
-export default function tabs() {
-  const elTabs = document.querySelectorAll('[role="tablist"]');
+var mqw = window.matchMedia("(max-width:700px)");
 
-  forEach(elTabs, element => new Tabs().registerDom(element))
+function tabsNoJS(){
+
+  var tablist = document.getElementsByClassName('tabs')[0];
+
+  if (mqw.matches) {
+    console.log('< 700px');
+    tablist.classList.add('no-js');
+  } else {
+    console.log('> 700px');
+    tablist.classList.remove('no-js');
+  }
+
 }
+
+window.addEventListener("resize", tabsNoJS);
