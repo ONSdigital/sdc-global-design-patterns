34a12d1ccb33519006073cc425900973b5fae061
boxadesign <info@boxadesign.co.uk>
2018-10-25 14:06:23 +0100

    Cleaned up table scss and added more examples

--- components/table/table.config.js
+++ components/table/table.config.js
@@ -5,15 +5,40 @@ module.exports = {
   "variants": [
     {
       "name": "default",
-      "label": "Basic table"
+      "label": "Basic table",
+      "context": {
+        "caption": "A simple table with a caption",
+      }
     },
     {
       "name": "data-dense",
       "label": "Compact table for data heavy applications.",
       "context": {
-        "table-class": " table__dense",
+        "table-class": " table--dense",
         "caption": "Use this table variant for for data heavy applications",
         "datadense": "true"
+      }
+    },
+    {
+      "name": "numeric",
+      "label": "Numeric table",
+      "context": {
+        "caption": "A basic table with numeric values"
+      }
+    },
+    {
+      "name": "headers",
+      "label": "Two header table",
+      "context": {
+        "caption": "A table with column and row headers"
+      }
+    },
+    {
+      "name": "footers",
+      "label": "Table with a footer",
+      "context": {
+        "tfoot": "true",
+        "caption": "A basic table showing a footer"
       }
     }
   ]
