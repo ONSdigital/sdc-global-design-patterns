bb92106cdc0a26bdefce22836ea2ceb76b8ef55f
jrbarnes9 <jrbarnes9@hotmail.com>
2018-11-22 11:16:19 +0000

    added contained accordion variant

--- components/accordion/accordion.config.js
+++ components/accordion/accordion.config.js
@@ -2,15 +2,23 @@ module.exports = {
   title: 'Accordion',
   status: 'ready',
   default: 'accordion',
+  preview: '@preview__full-width',
   collated: true,
   'variants': [{
     'label':'Accordion',
     'name':'accordion',
   }, {
-    'label':'Accordion with preview',
-    'name':'accordion-with-preview',
+    'label':'Accordion controls',
+    'name':'accordion-controls',
     'context': {
       'show_all': true
+    }
+  }, {
+    'label':'Accordion contained',
+    'name':'accordion-contained',
+    'context': {
+      'collapsible_class':' collapsible--contained',
+      'complex': true
     }
   }]
 };
