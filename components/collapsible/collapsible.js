fcfeb862f379cd942905b4345a1cf32e8ad97668
Matt Simon <info@boxadesign.co.uk>
2018-11-26 11:52:52 +0000

    fixed aria-controls id

--- components/collapsible/collapsible.js
+++ components/collapsible/collapsible.js
@@ -66,7 +66,7 @@ class Collapsible {
   registerTitle(element, index) {
     const componentId = this.component.getAttribute('id')
     element.setAttribute('id', componentId + '-collapsible-title-' + index)
-    element.setAttribute(attrControls, 'collapsible-body-' + index)
+    element.setAttribute(attrControls, componentId + '-collapsible-body-' + index)
     element.setAttribute(attrExpanded, 'false')
     element.setAttribute(attrSelected, 'false')
 
