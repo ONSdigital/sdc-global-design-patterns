0643300e39fc6772930a19f12215baadbb94e070
Ben Armstrong <benjamin.armstrong@ons.gov.uk>
2018-11-27 17:45:52 +0000

    Experimented with re-structuring to give 1-patterns and 2-elements. Added sign-in as a pattern to demonstrate pattern documentation.

---
+++ components/01-patterns/sign-in/login.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  title: 'Sign in',
+  status: 'ready',
+  default: 'login',
+  collated: false,
+  'variants': [{
+    'label':'Sign in',
+    'name':'sign-in',
+  }, {
+    'label':'Step 2',
+    'name':'step-2',
+    'context': {
+    }
+  },{
+    'label':'Forgotten password',
+    'name':'forgot-password',
+    'context': {
+    }
+  },{
+    'label':'Check your email',
+    'name':'check-email',
+    'context': {
+    }
+  }]
+};
---
+++ components/01-patterns/sign-in/readme.md
@@ -0,0 +1,4 @@
+## Rationale
+
+### General
+All services requiring a sign in process should replicate this user flow.
