536e11f25c37067525d6a9b5790a7405f6b76289
jrbarnes9 <jrbarnes9@hotmail.com>
2018-11-27 10:00:11 +0000

    new question pattern

---
+++ components/question/question.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  'title': 'Question',
+  collated: false, // Pulls all variants into one summary
+  'status': 'wip',
+  variants: [
+    {
+      name: 'default',
+      label: 'Single',
+      context: {
+        section_title: 'Section title',
+        question_id: 'central-heating-question',
+        question_number: '1',
+        question_title: 'What type of central heating does this house have?',
+        fieldset_legend: 'What type of central heating do you have?',
+        field_label: 'Select all that apply:',
+        or: 'Or',
+        options: [
+          { option_text: 'Gas', 'option_value': 'gas', 'label_for': 'gas', 'label_text': 'Gas', 'label_inline': true},
+          { option_text: 'Electric', 'option_value': 'electric', 'label_for': 'electric', 'label_text': 'Electric', 'label_inline': true},
+          { option_text: 'Solid fuel', 'option_value': 'solid-fuel', 'label_for': 'solid-fuel', 'label_text': 'Solid fuel', 'label_inline': true},
+          { option_text: 'Other', 'option_value': 'other', 'label_for': 'other', 'label_text': 'Other', 'label_inline': true}
+        ],
+        or_option_text:'No central heating',
+        or_option_value:'no central heating',
+        or_deselect_adjective: 'deselected',
+        or_deselect_message: 'Selecting this will uncheck all other checkboxes'
+      }
+    },
+    {
+      name: "repeating",
+      label: "Repeating", 
+      context: {
+        question_class: "question--repeatinganswer",
+      }
+    }
+  ]
+}
