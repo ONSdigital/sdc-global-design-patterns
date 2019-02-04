57fec39a72125d9840ff8fc0fa40fddbdc47a0db
Ben Meyrick <bameyrick@gmail.com>
2019-02-04 13:23:01 +0000

    2139 language switching (#240)
    * Change to retrigger netlify builds
    
    * Revert "Change to retrigger netlify builds"
    
    This reverts commit 717dd212f87ffcc8f5db66711cc2825e44b74a09.
    
    * Added desktop language switcher
    
    * Please start deploying again netlify
    
    * Update language selector to match design system

--- components/language/language.config.js
+++ components/language/language.config.js
@@ -1,23 +1,114 @@
 module.exports = {
-  title: "Language",
-  label: "Language selection",
+  title: 'Language',
+  label: 'Language selection',
   collated: true,
-  status: "wip",
-  default: "toggle-en",
-  preview: "@preview__page",
-  variants: [{
-    'name': 'toggle-en',
-    'label': 'Toggle',
-    'context': {
-      'lang_link': '#eng',
-      'lang_link_text': 'English',
+  status: 'wip',
+  default: 'toggle-en',
+  preview: '@preview__page',
+  variants: [
+    {
+      'name': 'toggle-en',
+      'label': 'Toggle English (Ireland)',
+      'context': {
+        'languages': [
+          {
+            'url': '#',
+            'ISOCode': 'en-GB',
+            'text': 'English',
+            'buttonAriaLabel': 'Language selector. Current language: English',
+            'chooseLanguage': 'Choose language',
+            'current': true,
+          },
+          {
+            'url': '#',
+            'ISOCode': 'ga',
+            'text': 'Gaeilge',
+            'buttonAriaLabel': 'Roghnóir teanga. Teanga reatha: Gaeilge',
+            'chooseLanguage': 'Roghnaigh teanga',
+            'current': false,
+          },
+          {
+            'url': '#',
+            'ISOCode': 'sco',
+            'text': 'Ulstèr-Scotch',
+            'buttonAriaLabel': 'Leid selectgor. Current leid: Ulstèr-Scotch',
+            'chooseLanguage': 'Wale leid',
+            'current': false,
+          },
+        ],
+      },
     },
-  },{
-    'name': 'toggle-cy',
-    'label': 'CY: Toggle',
-    'context': {
-      'lang_link': '#cym',
-      'lang_link_text': 'Cymraeg',
-    }
-  }]
+    {
+      'name': 'toggle-cy',
+      'label': 'Toggle Cymraeg',
+      'context': {
+        'languages': [
+          {
+            'url': '#',
+            'ISOCode': 'en-GB',
+            'text': 'English',
+            'buttonAriaLabel': 'Language selector. Current language: English',
+            'chooseLanguage': 'Choose language',
+            'current': false,
+          },
+          {
+            'url': '#',
+            'ISOCode': 'cy',
+            'text': 'Cymraeg',
+            'buttonAriaLabel': 'Dewisydd iaith. Iaith gyfredol: Cymraeg',
+            'chooseLanguage': 'Dewiswch iaith',
+            'current': true,
+          },
+        ],
+      },
+    },
+    {
+      'name': 'toggle-many',
+      'label': 'Toggle Many',
+      'context': {
+        'currentLanguage': {
+          'url': '#',
+          'ISOCode': 'en-GB',
+          'text': 'English',
+          'buttonAriaLabel': 'Language selector. Current language: English',
+          'chooseLanguage': 'Choose language',
+          'current': true,
+        },
+        'languages': [
+          {
+            'url': '#',
+            'ISOCode': 'en-GB',
+            'text': 'English',
+            'buttonAriaLabel': 'Language selector. Current language: English',
+            'chooseLanguage': 'Choose language',
+            'current': true,
+          },
+          {
+            'url': '#',
+            'ISOCode': 'cy',
+            'text': 'Cymraeg',
+            'buttonAriaLabel': 'Dewisydd iaith. Iaith gyfredol: Cymraeg',
+            'chooseLanguage': 'Dewiswch iaith',
+            'current': false,
+          },
+          {
+            'url': '#',
+            'ISOCode': 'ga',
+            'text': 'Gaeilge',
+            'buttonAriaLabel': 'Roghnóir teanga. Teanga reatha: Gaeilge',
+            'chooseLanguage': 'Roghnaigh teanga',
+            'current': false,
+          },
+          {
+            'url': '#',
+            'ISOCode': 'sco',
+            'text': 'Ulstèr-Scotch',
+            'buttonAriaLabel': 'Leid selectgor. Current leid: Ulstèr-Scotch',
+            'chooseLanguage': 'Wale leid',
+            'current': false,
+          },
+        ],
+      },
+    },
+  ],
 }
